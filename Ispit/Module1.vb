57432b8eaa45d76bdbf5de0c3f066b80c6000adb
Nikola Savanovic <n.savanovic@yahoo.com>
2021-10-14 18:10:19 +0200

    stage

--- Ispit/Module1.vb
+++ Ispit/Module1.vb
@@ -1,7 +1,25 @@
 ﻿Module Module1
 
 	Sub Main()
-		Console.WriteLine("Pozdrav na ispitu")
+		Randomize()
+		Dim a(19) As Integer
+		Dim b(19) As Integer
+
+		For i = 0 To 19
+			a(i) = Rnd() * 50 + 10
+			b(i) = Rnd() * 50 + 10
+		Next
+
+		For i = 0 To 19
+			Console.Write(a(i).ToString() + " ")
+		Next
+		Console.WriteLine()
+		For i = 0 To 19
+			Console.Write(b(i).ToString() + " ")
+		Next
+
+
+
 		Console.ReadKey()
 	End Sub
 
