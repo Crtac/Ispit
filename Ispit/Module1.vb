ed5fd6a9cea425f32f7b74c22a453b37c1b7ba77
Nikola Savanovic <n.savanovic@yahoo.com>
2021-10-14 19:18:06 +0200

    New stage

--- Ispit/Module1.vb
+++ Ispit/Module1.vb
@@ -1,37 +1,7 @@
 ﻿Module Module1
 
 	Sub Main()
-		Randomize()
-		Dim a(19) As Integer
-		Dim b(19) As Integer
 
-		For i = 0 To 19
-			a(i) = Rnd() * 50 + 10
-			b(i) = Rnd() * 50 + 10
-		Next
-
-		For i = 0 To 19
-			Console.Write(a(i).ToString() + " ")
-		Next
-		Console.WriteLine()
-		For i = 0 To 19
-			Console.Write(b(i).ToString() + " ")
-		Next
-		Console.WriteLine()
-		Dim imaZajednickih As Boolean = False
-		Console.Write("Zajednički su :> ")
-		For i = 0 To 19
-			For j = 0 To 19
-				If a(i) = b(j) Then
-					Console.Write(a(i).ToString + " ")
-					imaZajednickih = True
-				End If
-			Next
-		Next
-
-		If imaZajednickih = False Then
-			Console.WriteLine("Nema zajedničkih")
-		End If
 
 		Console.ReadKey()
 	End Sub
